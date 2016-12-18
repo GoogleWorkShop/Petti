eab6a396c4956b147866adb7403b56602e6a8ea3
razmay89 <razmay89@gmail.com>
2016-12-18 20:56:29 +0200

    pet register and user

--- app/src/main/java/com/firebase/petti/petti/RegistrationActivity.java
+++ app/src/main/java/com/firebase/petti/petti/RegistrationActivity.java
@@ -1,24 +1,109 @@
 package com.firebase.petti.petti;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private static final int SELECT_PICTURE = 100;
+    private static final String TAG = "RegistrationActivity";
+    EditText nameView;
+    EditText ageView ;
+    Button uploadButton;
+    ImageView petImage;
+    final String[] dog_type = new String[1];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        nameView = (EditText) findViewById(R.id.name);
+        ageView = (EditText) findViewById(R.id.age);
+        uploadButton = (Button) findViewById(R.id.uploadButton);
+        petImage = (ImageView) findViewById(R.id.pet_image);
+
 
         Spinner spinner = (Spinner) findViewById(R.id.pet_type_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.dog_types_array, android.R.layout.simple_spinner_item);
-            // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.dog_ype_spinner_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                dog_type[0] = adapterView.getItemAtPosition(pos).toString();
+                Toast.makeText(adapterView.getContext(), "type :" + dog_type[0], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+
+
+    }
+
+
+    public void uploadImageMethod(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // Get the path from the Uri
+                    String path = getPathFromURI(selectedImageUri);
+                    Log.i(TAG, "Image Path : " + path);
+                    // Set the image in ImageView
+                    petImage.setImageURI(selectedImageUri);
+
+                }
+            }
+        }
+    }
+
+
+    public String getPathFromURI(Uri contentUri) {
+        String res = null;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if (cursor.moveToFirst()) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+        }
+        cursor.close();
+        return res;
+    }
+
+
+    public void startUserRegistrationActivity(View view) {
+        Intent intent = new Intent(this, UserRegistrationActivitey.class);
+        startActivity(intent);
     }
 }
