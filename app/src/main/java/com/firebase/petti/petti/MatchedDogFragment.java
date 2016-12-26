96b8a442537e50fd2f7b7855144fd9389e078013
Nir Bar-Joseph <nir.mingus@gmail.com>
2016-12-26 10:32:32 +0200

    started food notifs

--- app/src/main/java/com/firebase/petti/petti/MatchedDogFragment.java
+++ app/src/main/java/com/firebase/petti/petti/MatchedDogFragment.java
@@ -4,21 +4,16 @@ package com.firebase.petti.petti;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
-
-import static com.firebase.petti.petti.R.id.imageView;
 
 
 /**
@@ -44,7 +39,7 @@ public class MatchedDogFragment extends Fragment {
         dogName = parentActivity.getDogName();
         imageUrl = parentActivity.getImageUrl();
 
-        View rootView = inflater.inflate(R.layout.fragment_matched, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_matched_dog, container, false);
 
         TextView titleTextView = (TextView) rootView.findViewById(R.id.matched_dog_detail);
         titleTextView.setText(dogName);
--- app/src/main/java/com/firebase/petti/petti/MatchesFragment.java
+++ app/src/main/java/com/firebase/petti/petti/MatchesFragment.java
@@ -19,6 +19,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.petti.petti.utils.GPSTracker;
@@ -32,6 +33,8 @@ import java.util.ArrayList;
  */
 public class MatchesFragment extends Fragment {
 
+    View rootView;
+
     GridViewAdapter mMatchesAdapter;
     boolean bark;
     private final static String DEFAULT_PREFERENCE_STRING = "com.firebase.petti.petti_preferences";
@@ -44,9 +47,9 @@ public class MatchesFragment extends Fragment {
     };
     private static final int INITIAL_REQUEST = 1337;
 
+
     public MatchesFragment() {
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -90,7 +93,7 @@ public class MatchesFragment extends Fragment {
                 "Bark is: " + bark,
                 Toast.LENGTH_LONG).show();
 
-        View rootView = inflater.inflate(R.layout.fragment_matches, container, false);
+        rootView = inflater.inflate(R.layout.fragment_matches, container, false);
 
         GridView gridView = (GridView) rootView.findViewById(R.id.gridview_matches);
         gridView.setAdapter(mMatchesAdapter);
@@ -183,33 +186,33 @@ public class MatchesFragment extends Fragment {
 
             //TODO DELETE FROM HERE
 
-            String[] a = {"a", "a",
-                    "http://pngimg.com/upload/dog_PNG2416.png"};
-            mMatchesArray.add(a);
-
-            String[] b = {"b", "b",
-                    "https://s-media-cache-ak0.pinimg.com/564x/fe/a6/dd/fea6dd493a862241952066fea699feaa.jpg"};
-            mMatchesArray.add(b);
-
-            String[] c = {"c", "c",
-                    "https://s-media-cache-ak0.pinimg.com/564x/aa/21/8e/aa218e0d81d51178ab68f65ef759eb11.jpg"};
-            mMatchesArray.add(c);
-
-            String[] d = {"d", "d",
-                    "https://s-media-cache-ak0.pinimg.com/originals/d9/1b/ee/d91bee03625f15e36020de6d9969a30b.png"};
-            mMatchesArray.add(d);
-
-            String[] e = {"e", "e",
-                    "http://pngimg.com/upload/dog_PNG2422.png"};
-            mMatchesArray.add(e);
-
-            String[] f = {"f", "f",
-                    "http://pngimg.com/upload/dog_PNG149.png"};
-            mMatchesArray.add(f);
-
-            String[] g = {"g", "g",
-                    "http://pngimg.com/upload/dog_PNG192.png"};
-            mMatchesArray.add(g);
+//            String[] a = {"a", "a",
+//                    "http://pngimg.com/upload/dog_PNG2416.png"};
+//            mMatchesArray.add(a);
+//
+//            String[] b = {"b", "b",
+//                    "https://s-media-cache-ak0.pinimg.com/564x/fe/a6/dd/fea6dd493a862241952066fea699feaa.jpg"};
+//            mMatchesArray.add(b);
+//
+//            String[] c = {"c", "c",
+//                    "https://s-media-cache-ak0.pinimg.com/564x/aa/21/8e/aa218e0d81d51178ab68f65ef759eb11.jpg"};
+//            mMatchesArray.add(c);
+//
+//            String[] d = {"d", "d",
+//                    "https://s-media-cache-ak0.pinimg.com/originals/d9/1b/ee/d91bee03625f15e36020de6d9969a30b.png"};
+//            mMatchesArray.add(d);
+//
+//            String[] e = {"e", "e",
+//                    "http://pngimg.com/upload/dog_PNG2422.png"};
+//            mMatchesArray.add(e);
+//
+//            String[] f = {"f", "f",
+//                    "http://pngimg.com/upload/dog_PNG149.png"};
+//            mMatchesArray.add(f);
+//
+//            String[] g = {"g", "g",
+//                    "http://pngimg.com/upload/dog_PNG192.png"};
+//            mMatchesArray.add(g);
 
             //TODO TO HERE
 
@@ -219,10 +222,22 @@ public class MatchesFragment extends Fragment {
 
         @Override
         protected void onPostExecute(ArrayList<String[]> result) {
+
+            GridView gridView = (GridView) rootView.findViewById(R.id.gridview_matches);
+            TextView textView = (TextView) rootView.findViewById(R.id.no_matches_str);
+
             if (result != null) {
                 mMatchesAdapter.clear();
                 mMatchesAdapter.refresh(result);
                 // New data is back from the server.  Hooray!
+            }
+            if(mMatchesAdapter.isEmpty()){
+                gridView.setVisibility(View.GONE);
+                textView.setVisibility(View.VISIBLE);
+
+            } else {
+                gridView.setVisibility(View.VISIBLE);
+                textView.setVisibility(View.GONE);
             }
         }
     }
